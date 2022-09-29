805653c2e6e36b75f06df180e1aad24bba9546ce
TeddyWalsh <tjw6911@g.rit.edu>
2022-09-28 20:25:58 -0400

    Instilled the monster with a sence of fear (will run away from the player if scared, but no current way to make them afraid)

--- Assets/Scripts/IGME_320/movement.cs
+++ Assets/Scripts/IGME_320/movement.cs
@@ -13,6 +13,19 @@ public class movement : MonoBehaviour
     // Get a reference to the player
     public GameObject player;
 
+    // If they are afraid and running away from the player
+    bool scared = false;
+
+    // Keeps track of how many fixedUpdate()s until its not scared anymore
+    int scaredTimer = 0;
+
+    // How long it takes the enemy for them to not be scared anymore
+    const int SCARED_TIME = 300;
+
+    // Distance away that the monster will try to run from the player
+    const int SCARE_RUN_DISTANCE = 5;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,7 +40,38 @@ public class movement : MonoBehaviour
 
     void Move()
     {
-        // Update target position to be the player's position
-        agent.SetDestination(player.transform.position);
+        // Update the target position
+
+        // Check if they're scared of the player or not
+        if (!scared) // They are not scared of the player and should be moving towards them
+        {
+            agent.SetDestination(player.transform.position);
+        }
+        else // They are scared of the player and should be moving away from them
+        {
+            // Calculate a new target position that is in the oposite direction of the player
+            Vector3 directionAwayFromPlayer = player.transform.position - transform.position;
+            Vector3 targetLocation = directionAwayFromPlayer.normalized * SCARE_RUN_DISTANCE;
+            agent.SetDestination(targetLocation);
+        }
+    }
+
+    // Called every frame
+    void UpdateScared()
+    {
+        // Decrease the scared timer down by 1 to 0
+        if (scaredTimer > 0)
+        {
+            scaredTimer -= 1;
+        }
+
+        // Check if its still scared
+        scared = scaredTimer > 0;
+    }
+
+    void Spook()
+    {
+        scaredTimer = SCARED_TIME;
+        scared = true;
     }
 }
