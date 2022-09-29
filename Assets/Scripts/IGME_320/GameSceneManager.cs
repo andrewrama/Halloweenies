4823e8ed425586acd0f9e370e62ba610dec08ff5
Gia Lopez <gnicolelopez@gmail.com>
2022-09-29 16:39:25 -0400

    testing scene changes

--- Assets/Scripts/IGME_320/GameSceneManager.cs
+++ Assets/Scripts/IGME_320/GameSceneManager.cs
@@ -7,17 +7,7 @@ public class GameSceneManager : MonoBehaviour
 {
     public void LoadScene()
     {
-        SceneManager.LoadScene(2);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+ 
 }
