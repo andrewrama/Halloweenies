bdd211d5529bfd0830dbd0861b67b670c086c7bd
Gia Lopez <gnicolelopez@gmail.com>
2022-09-28 22:14:22 -0400

    Working on Scene Management

--- Assets/Scripts/IGME_320/GameReset.cs
+++ Assets/Scripts/IGME_320/GameReset.cs
@@ -2,6 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+enum GameStates
+{
+    Title,
+    Game,
+    Pause,
+    WinScreen,
+    GameOver
+}
+
 public class GameReset : MonoBehaviour
 {
     [SerializeField] private bool gameIsWon;
@@ -9,22 +18,42 @@ public class GameReset : MonoBehaviour
     [SerializeField] private GameObject player;
     [SerializeField] private Vector3[] enemySpawns;
 
+    //Fields for current State
+    private GameStates currentState;
+    private GameStates prevState;
+
     // Start is called before the first frame update
     void Start()
     {
         //Intialize Variables
         enemySpawns = new Vector3[2] { new Vector3(30.0f, 0.0f, -28.0f), new Vector3(-30.0f, 0.0f, -28.0f) };
-        
+
+        //makes the title screen the first scene
+        currentState = GameStates.Title;
+
         ResetGame();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Game State loop
+        switch (currentState)
+        {
+            //Title Screen
+            case GameStates.Title:
+                break;
+
+
+        }
+
+
         if(gameIsWon)
         {
             ResetGame();
         }
+
+
     }
 
     void EndGame()
