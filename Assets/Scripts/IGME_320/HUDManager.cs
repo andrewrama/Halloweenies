5531044aee3d5e9a9811ab47e00d632619fc99e4
NarraAtor <eff3587@rit.edu>
2022-11-02 16:43:20 -0400

    Developed an extendable HUD prototype. Pseudo code in HUDManager script

--- Assets/Scripts/IGME_320/HUDManager.cs
+++ Assets/Scripts/IGME_320/HUDManager.cs
@@ -1,21 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class HUDManager : MonoBehaviour
 {
     [SerializeField] private GameObject HUD;
-    [SerializeField] private GameObject HUDCandyDisplay;
-    [SerializeField] private GameObject[] HUDKeyDisplay;
+    [SerializeField] private Image HUDCandyDisplay;
+    [SerializeField] private Image[] HUDKeyDisplayImages;
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach (Image keyImage in HUDKeyDisplayImages)
+        {
+            keyImage.color = Color.black;
+        }
+
+        HUDCandyDisplay.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void SetCandyVisible()
+    {
+        HUDCandyDisplay.enabled = true;
+    }
+    public void SetCandyInvisible()
+    {
+        HUDCandyDisplay.enabled = false;
+    }
+    public void SetKeyVisible(int keyIndex)
+    {
+        //TODO: set key source images here
+        // -1 is reserved for the end door, which will be the last index
+        switch(keyIndex)
+        {
+            case -1:
+                // set end key image here
+                // HUDKeyDisplayImages[HUDKeyDisplayImages.Length - 1].sprite = sourceImg;
+                HUDKeyDisplayImages[HUDKeyDisplayImages.Length - 1].color = Color.white;
+                return;
+            case 0:
+                // set first key image here
+                // HUDKeyDisplayImages[keyIndex].sprite = sourceImg;
+                break;
+        }
+
+        HUDKeyDisplayImages[keyIndex].color = Color.white;
+
     }
 }
